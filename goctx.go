58f2b29788e6f3c536649ea2742c01c69749b7fd
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2019-01-08 00:10:35 +0900

    Unified function call

--- goctx.go
+++ goctx.go
@@ -6,23 +6,51 @@ import (
 )
 
 type Owner struct {
-	Ctx	context.Context
-	Cancel	context.CancelFunc
-	Mux	sync.Mutex
+	ctx	context.Context
+	cancel	context.CancelFunc
+	mux	sync.Mutex
 }
 
 type Worker struct {
-	Ctx	context.Context
-	Cancel	context.CancelFunc
-	Mux	*sync.Mutex
-}
-
-func (self *Owner)NewWorker() Worker {
-	ctx, _ := context.WithCancel(self.Ctx)
-	return Worker{Ctx:ctx, Cancel:self.Cancel, Mux:&self.Mux}
+	ctx	context.Context
+	cancel	context.CancelFunc
+	mux	*sync.Mutex
 }
 
 func NewOwner() Owner {
 	ctx, cancel := context.WithCancel(context.Background())
-	return Owner{Ctx:ctx, Cancel:cancel, Mux:sync.Mutex{}}
+	return Owner{ctx:ctx, cancel:cancel, mux:sync.Mutex{}}
+}
+
+func (self *Owner)NewWorker() Worker {
+	ctx, _ := context.WithCancel(self.ctx)
+	return Worker{ctx:ctx, cancel:self.cancel, mux:&self.mux}
+}
+
+func (self *Owner) Lock() {
+	self.mux.Lock()
+}
+
+func (self *Owner) Unlock() {
+	self.mux.Unlock()
+}
+
+func (self *Owner) Cancel() {
+	self.cancel()
+}
+
+func (self *Worker) Lock() {
+	self.mux.Lock()
+}
+
+func (self *Worker) Unlock() {
+	self.mux.Unlock()
+}
+
+func (self *Worker) Cancel() {
+	self.cancel()
+}
+
+func (self *Worker) RecvCancel() <-chan struct{} {
+	return self.ctx.Done()
 }
